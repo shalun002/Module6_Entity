554c48cfe1707b72d8180db1d15387fc3a53e049
Блиновс <@242_7>
2018-07-16 21:26:44 +0600

    1111

--- Module6_2/Program.cs
+++ Module6_2/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Module6_2
+namespace Module6_2  //CodeFirst Automatic
 {
     class Program
     {
---
+++ Module6_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module6_3.Modul;
+
+namespace Module6_3 // Data Base First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MCSEntities())
+            {
+                foreach (var stopReason in db.AccessTabs)
+                {
+                    Console.WriteLine(stopReason.strTabName + "\t\t\t\t\t" + stopReason.intTabID);
+                }
+            }
+        }
+    }
+}
